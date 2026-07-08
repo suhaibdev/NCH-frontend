22e80f85197c2013c391baf04ace2614a0eac101
suhaibdev <khalidsuhaib4@gmail.com>
2026-07-08 14:50:45 +0530

    5 commit

--- src/pages/Customer/CustomersPage.jsx
+++ src/pages/Customer/CustomersPage.jsx
@@ -1,5 +1,5 @@
 // File: client/src/pages/CustomersPage.js
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
