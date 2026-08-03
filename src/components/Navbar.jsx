421f4e3dca32ed34c7c57aa3cd3132de0c9418f0
suhaibdev <khalidsuhaib4@gmail.com>
2026-08-03 12:13:14 +0530

    Add JWT authentication and admin dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./Navbar.css";
 import { Link, useNavigate } from 'react-router-dom';
 import { getUser, isAuthenticated, logout } from '../config/auth';
 
